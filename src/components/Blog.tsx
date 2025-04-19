00e0b695215b5328071b0445f0f81391b0544cdc
Rutuja1923 <rutujajirage19@gmail.com>
2025-04-19 09:09:54 +0530

    feat: like blogs & state lifting

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { blogData } from "./blogData";
+import BlogCard from "./BlogCard";
 
 type Blog = {
   id: number;
@@ -8,6 +10,13 @@ type Blog = {
 };
 
 const Blog: React.FC = () => {
+  const [likedBlogs, setLikedBlogs] = useState<number[]>([]);
+  const handleLike = (id: number) => {
+    setLikedBlogs((prev) =>
+      prev.includes(id) ? prev.filter((blogId) => blogId !== id) : [...prev, id]
+    );
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4 m-auto">
       <h1 className="text-white text-center text-2xl font-bold mt-4 mb-6">
@@ -15,20 +24,12 @@ const Blog: React.FC = () => {
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2">
         {blogData.map((blog: Blog) => (
-          <div
-            className="bg-[#313131] text-white p-2 m-2 rounded-[12px] w-[80%] mx-auto"
+          <BlogCard
             key={blog.id}
-          >
-            <img
-              src={blog.cover}
-              alt="image"
-              className="w-full h-[200px] object-cover rounded-md"
-            />
-            <div className="mt-2">
-              <h2 className="text-lg font-semibold">{blog.title}</h2>
-              <h4 className="text-sm text-gray-300">By {blog.author}</h4>
-            </div>
-          </div>
+            {...blog}
+            onLike={handleLike}
+            isLiked={likedBlogs.includes(blog.id)}
+          />
         ))}
       </div>
     </div>
